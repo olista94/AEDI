c870a81116bc0ae245a7eae9fbe318d03ab8da05
Oscar Lista <43946678+olista94@users.noreply.github.com>
2021-02-25 13:14:01 +0100

    Add files via upload

---
+++ Actividad2/src/actividad2/Actividad2.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad2;
+
+/**
+ *
+ * @author olista94
+ */
+public class Actividad2 {
+
+    class Nodo{
+        private int elemento;
+        private Nodo siguiente;
+
+        public Nodo(int elemento, Nodo siguiente) {
+            this.elemento = elemento;
+            this.siguiente = siguiente;
+        }
+
+        public int getElemento() {
+            return elemento;
+        }
+
+        public Nodo getSiguiente() {
+            return siguiente;
+        }
+
+        public void setElemento(int elemento) {
+            this.elemento = elemento;
+        }
+
+        public void setSiguiente(Nodo siguiente) {
+            this.siguiente = siguiente;
+        }
+        
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
